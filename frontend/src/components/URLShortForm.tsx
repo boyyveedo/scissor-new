0c7d6d385a597c9809d28c8b18c52ed5e587873b
boyyveedo <talktoboyveedo@gmail.com>
2024-08-09 15:25:02 +0100

    form updated

--- frontend/src/components/URLShortForm.tsx
+++ frontend/src/components/URLShortForm.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 import { SERVER_ENDPOINTS } from '../config';
 
-const apiUrl = `${SERVER_ENDPOINTS}/shorten`;
+const apiUrl = 'https://scissor-tvhk.onrender.com/shorten'
 
 const URLShortForm: React.FC = () => {
     const [destination, setDestination] = useState<string>('');
