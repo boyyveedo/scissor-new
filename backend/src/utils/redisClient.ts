635afbf24ad286c210a3f4aa9351aa2932084881
boyyveedo <talktoboyveedo@gmail.com>
2024-08-09 17:39:54 +0100

    newest changes

--- backend/src/utils/redisClient.ts
+++ backend/src/utils/redisClient.ts
@@ -1,9 +1,11 @@
-
-
 import { createClient } from 'redis';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const redisClient = createClient({
-    url: 'redis://localhost:6379'
+    url: process.env.REDIS_URL
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
